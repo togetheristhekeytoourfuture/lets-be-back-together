3e76c522b09db9e500f350ac70ad71abda8744e7
iwantyou <vivianho.monsta@gmail.com>
2026-06-07 21:35:46 +0800

    Increase background music volume to maximum

--- yes-script.js
+++ yes-script.js
@@ -5,7 +5,7 @@ window.addEventListener('load', () => {
 
     // Autoplay music (works since user clicked Yes to get here)
     const music = document.getElementById('bg-music')
-    music.volume = 0.3
+    music.volume = 1.0
     music.play().catch(() => {})
     musicPlaying = true
     document.getElementById('music-toggle').textContent = '🔊'
